0490e1729c77885c86a9e21b743c8f07b38a1f53
jaoc1811 <jaoc1811@gmail.com>
2018-10-25 12:56:04 -0400

    Finished Edge class

--- projects/project1/Edge.java
+++ projects/project1/Edge.java
@@ -1,3 +1,62 @@
-public class Edge {
-  
+public abstract class Edge<T> {
+
+    private String id;
+    private T data;
+    private double weight;
+    private Vertex<U> v1;
+    private Vertex<U> v2;
+
+    /**
+     * Create new edge with a id, data and weight
+     * @param id // Edge's id
+     * @param data // Edge's data
+     * @param p // Edge's weight
+     * @return  // Edge
+     */
+    protected Edge<T> createEdge(String id,T data, double p){
+
+        Edge<T> e = new Edge<T>();
+        e.id = id;
+        e.data = data;
+        e.weight = weight;
+        
+        return e;
+    }
+
+    /**
+     * Get the weight of an egde
+     * @param e // Edge to consider
+     * @return // Edge's weight
+     */
+    protected double getWeight(Edge<T> e){
+
+        return e.weight;
+    }
+
+    /**
+     * Get id of an Edge
+     * @param e // Edge to consider
+     * @return // Edge's id
+     */
+    protected String getId(Edge<T> e){
+
+        return e.id;
+    }
+
+    /**
+     * Get data of an Edge
+     * @param e // Edge to consider
+     * @return // Edge's data
+     */
+    protected T getData(Edge<T> e){
+
+        return e.data;
+    }
+    
+    /**
+     * Get representation of an egde in string
+     * @param e // Egde to consider
+     * @return // Edge's representation
+     */
+    protected abstract String toString(Edge<T> e);
 }
