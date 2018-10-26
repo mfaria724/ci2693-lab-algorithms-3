bdd94666b1fb7468510e9bb0f367fb74de893dac
Manuel Faria <mfaria@wayuinc.com>
2018-10-26 17:53:18 -0400

    Implementations of almost all methods in DirectedGraph

--- projects/project1/Vertex.java
+++ projects/project1/Vertex.java
@@ -22,51 +22,46 @@ public class Vertex<T> {
   public Vertex<T> createVertex(String id, T data, double weight){
 
     // Initialice vertexes.
-    Vertex<T> v = new Vertex<T>();
-    v.id = id;
-    v.data = data;
-    v.weight = weight;
+    this.id = id;
+    this.data = data;
+    this.weight = weight;
 
-    return v;
+    return this;
   };
 
   /**
    * Gets the Vertex's weight 
-   * @param v // Vertex's to evaluate.
    * @return // Vertex's weight
    */
-  public Integer getWeight(Vertex<T> v){
+  public Integer getWeight(){
     return v.weight;
   };
 
   /**
    * Gets the Vertex's id.
-   * @param v // Vertex to evaluate.
    * @return // Vertex's id.
    */
-  public String getId(Vertex<T> v){
-    return v.id;
+  public String getId(){
+    return this.id;
   }
 
   /**
    * Gets Vertex's data.
-   * @param v // Vertex to evaluate.
    * @return // Vertex data.
    */
-  public T getData(Vertex<T> v){
-    return v.data;
+  public T getData(){
+    return this.data;
   }
 
   /**
    * Gets the vertex's string representation.
-   * @param v // Vertex to evaluate.
    * @return // Vertex's string representation.
    */
-  public String toString(Vertex<T> v){
+  public String toString(){
     String vString = "";
-    vString = "Vertex's id: " + v.id + "\n";
-    vString = "Vertex's data: " + v.data + "\n";
-    vString = "Vertex's weight: " + v.weight + "/n";
+    vString = "Vertex's id: " + this.id + "\n";
+    vString = "Vertex's data: " + this.data + "\n";
+    vString = "Vertex's weight: " + this.weight + "/n";
     return vString;
   }
 
