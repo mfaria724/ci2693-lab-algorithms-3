775c3d125e9795fe0534624bbd4204b1237d1be5
Manuel Faria <mfaria@wayuinc.com>
2018-11-14 13:37:49 -0400

    Read files

--- projects/project2/EncontrarAgua.java
+++ projects/project2/EncontrarAgua.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.chrono.ThaiBuddhistEra;
+import java.util.Arrays;
 
 public class EncontrarAgua {
 
@@ -12,8 +14,8 @@ public class EncontrarAgua {
 
     if(args.length < 4){
 
-      readGraph(args[0]);
-      readCases(args[1]);
+      UndirectedGraph graph = readGraph(args[0]);
+      readCases(args[1], graph, args[2]);
 
     }else{
       System.out.println("Uso: java EncontrarAgua <grafo> <casos> <edif> <personas>");
@@ -21,11 +23,9 @@ public class EncontrarAgua {
 
   }
 
-  public static void readGraph(String file){
+  public static UndirectedGraph readGraph(String file){
 
-    // Initialice BufferReader
     BufferedReader reader = new BufferedReader(new FileReader(file));
-        
     UndirectedGraph graph = new UndirectedGraph();
 
     String line;
@@ -34,11 +34,72 @@ public class EncontrarAgua {
     line = reader.readLine();
     int edges = Integer.parseInt(line);
     
+    for(int i = 0; i < vertices; i++){
+      line = reader.readLine();
+      String[] data = line.split(" ");
+      int capacity = Integer.parseInt(data[1]);
+      int floors = Integer.parseInt(data[2]);
+      graph.addVertex(data[0], capacity, floors);
+    }
+
+    for(int i = 0; i < edges; i++){
+      line = reader.readLine();
+      String[] data = line.split(" ");
+      int capacity = Integer.parseInt(data[2]);
+      int distance = Double.parseDouble(data[3]);
+      graph.addSimpleEdge(i, capacity, distance, data[0], data[1]);
+    }
+
+    reader.close();
+    return graph;
 
   }
 
-  public static void readCases(String file){
+  public static void readCases(String file, UndirectedGraph graph, String origin){
 
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    UndirectedGraph cloneGraph;
+    String line;
+    String caseId = reader.readLine();
+
+    while(caseId != null){
+      cloneGraph = graph.clone();
+
+      line = reader.readLine();
+      int buildings = Integer.parseInt(line);
+      line = reader.readLine();
+      int afectedWays = Integer.parseInt(line);
+
+      boolean[] exists = cloneGraph.numVertices();
+      Arrays.fill(exists, false);
+
+      for(int i = 0; i < buildings; i++){
+        line = reader.readLine();
+        String[] data = line.split(" ");
+        
+        exists[cloneGraph.getVertexIndex(data[0])] = true;
+
+        if(data.length == 2){
+          int newFloor = Integer.parseInt(data[1]);
+          cloneGraph.editVertexFloor(data[0], newFloor);
+        }
+      }
+
+      for(int i = 0; i < exists.length; i++){
+        if(!exists[i]){
+          cloneGraph.deleteVertex(String.parseInt(i));
+        }
+      }
+
+      for(int i = 0; i < afectedWays; i++){
+        line = reader.readLine();
+        cloneGraph.deleteSimpleEdge(line);
+      }
+
+      line = reader.readLine();
+      caseId = reader.readLine();
+
+      cloneGraph.bellmanFord(origin);
+    }
   }
-
 }
